e1e9d5907923a5b4a14e277df8c04ed795114b4f
karanamswathi <karanamswathichowdary999@gmail.com>
2024-04-28 16:12:48 -0400

    four commit

--- src/service.js
+++ src/service.js
@@ -1,7 +1,7 @@
 // axiosInstance.js
 import axios from 'axios';
 
-const baseURL = 'https://sea-turtle-app-jgpa3.ondigitalocean.app/api/v1'; // replace with your API base URL
+const baseURL = 'https://stingray-app-9xbjs.ondigitalocean.app/api/v1'; // replace with your API base URL
 
 const Axios = axios.create({
   baseURL,
